bbd1b8c05d611b4cb781ad9cc4b7682bd65d0a20
Preetisam <preetikamilla@gmail.com>
2023-07-02 15:11:07 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import * as mdb from 'mdb-ui-kit';
+import { Input } from 'mdb-ui-kit';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('playlist.json')
+      .then(res => res.json())
+      .then(data => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to My Music App</h1>
+        </header>
+        {/* <div className="container">
+          {loading ? (
+            <div className="loader">Loading...</div>
+          ) : (
+            <div className="song-grid">
+              {data.map((item, index) => (
+                <div className="card" key={index}>
+                  <img src={item.cover} alt="" />
+                  <h3>{item.name}</h3>
+                  <p>{item.artist}</p>
+                </div>
+              ))}
+            </div>
+          )}
+        </div> */}
+        <div className='all-cards'>
+        <div className="card">
+          <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#!" className="btn btn-primary">Button</a>
+          </div>
+        </div>
+        <div className="card">
+          <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#!" className="btn btn-primary">Button</a>
+          </div>
+        </div>
+        <div className="card">
+          <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#!" className="btn btn-primary">Button</a>
+          </div>
+        </div>
+        <div className="card">
+          <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#!" className="btn btn-primary">Button</a>
+          </div>
+        </div>
+        </div>
+        <footer className="App-footer">
+          <p>&copy; 2023 Your Company. All rights reserved.</p>
+        </footer>
+      </div>
+
+    </>
   );
 }
 
